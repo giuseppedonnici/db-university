273ed576b57a431db25b59b3904c44a003202776
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-05-15 14:36:52 +0200

    Add: 2nd query of group by

--- group_by.sql
+++ group_by.sql
@@ -4,5 +4,9 @@ FROM `students`
 GROUP BY YEAR(`enrolment_date`):
 
 2. Contare gli insegnanti che hanno l ufficio nello stesso edificio
+SELECT COUNT(*) AS `teachers_in_the_office`, `office_number`
+FROM `teachers` 
+GROUP BY `office_number`;
+
 3. Calcolare la media dei voti di ogni appello d esame
 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
